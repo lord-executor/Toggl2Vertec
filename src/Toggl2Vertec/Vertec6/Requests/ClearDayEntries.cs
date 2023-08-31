76a4f43342db59c906f47a57bd48a66e1c554adf
Lukas Angerer <lukas.angerer@elca.ch>
2023-08-31 17:18:02 +0200

    Clearing day entries

--- src/Toggl2Vertec/Vertec6/Requests/ClearDayEntries.cs
+++ src/Toggl2Vertec/Vertec6/Requests/ClearDayEntries.cs
@@ -18,6 +18,17 @@ namespace Toggl2Vertec.Vertec6.Requests
 
         public bool Execute(XmlApiClient client)
         {
+            var dateStr = _date.ToDateString();
+            var dayEntries = new GetDayEntries(_date.Date, _ownerId).Execute(client);
+            var deletes = new Delete();
+
+            deletes.Items = dayEntries
+                .Where(x => x.ObjId != null)
+                .Select(x => x.ObjId.Value)
+                .ToList();
+
+            var result = client.Request(deletes).Result;
+
             return true;
         }
     }
